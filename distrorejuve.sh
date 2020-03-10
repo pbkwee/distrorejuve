671e990356892842ab1b0668a25e8d36f33f72b0
peter@rimuhosting.com <peter@rimuhosting.com>
2020-03-11 08:47:09 +1300

    permit software.virtualmin.com in apt sources.list

--- distrorejuve.sh
+++ distrorejuve.sh
@@ -398,7 +398,7 @@ function upgrade_precondition_checks() {
     fi
   fi
   if [ -f /etc/apt/sources.list ]; then
-    local otherrepos=$(egrep -iv '^ *#|^ *$|^ *[a-z].*ubuntu.com|^ *[a-z].*debian.org|^ *[a-z].*debian.net' /etc/apt/sources.list | egrep -v '^[[:space:]]*$' | head -n 1 )
+    local otherrepos=$(egrep -iv '^ *#|^ *$|^ *[a-z].*ubuntu.com|^ *[a-z].*debian.org|^ *[a-z].*debian.net|software.virtualmin.co' /etc/apt/sources.list | egrep -v '^[[:space:]]*$' | head -n 1 )
     if [ ! -z "$otherrepos" ]; then
       echo "dss:warn:/etc/apt/sources.list looks like it contains an unknown repository.  comment out before proceeding?: '$otherrepos'"
       # to find what repositories are in play
